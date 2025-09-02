d937c1dcb6e67835ac6081f64ef293e7669f7856
Danny Mickleburgh <dannymickleburgh@gmail.com>
2025-09-02 14:55:20 -0700

    fix gtm

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ import '@radix-ui/themes/styles.css';
 import { Theme } from '@radix-ui/themes';
 import { Urbanist } from 'next/font/google';
 import Footer from '@/components/Footer';
+import Script from 'next/script';
 
 const urbanist = Urbanist({ subsets: ['latin'], variable: '--font-urbanist', display: 'swap' });
 
@@ -13,6 +14,16 @@ export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
   return (
     <div className={`${urbanist.variable} font-sans`}>
+      {/* Google Tag Manager (loads after hydration) */}
+      {process.env.NEXT_PUBLIC_GTM_ID ? (
+        <Script id="gtm-init" strategy="afterInteractive">{`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');
+        `}</Script>
+      ) : null}
       <Theme appearance="dark" accentColor="violet" grayColor="mauve" radius="large" scaling="95%">
         <AnimatePresence mode="wait" initial={false}>
           <motion.div
