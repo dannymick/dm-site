5ae92bfe2b281ad3898a0d32252c54be27131a66
Danny Mickleburgh <dannymickleburgh@gmail.com>
2025-09-02 14:46:56 -0700

    add gtm

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -21,6 +21,10 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        {/* <!-- Google Tag Manager (noscript) --> */}
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NVHCLSCN"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        {/* <!-- End Google Tag Manager (noscript) --> */}
       </body>
     </Html>
   );
